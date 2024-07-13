9f4c51f2a639f348324cdf73f547b854f959c9c1
aliaaronyu1 <agyu@dons.usfca.edu>
2024-07-13 18:32:12 -0400

    WIP on redirect signin

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,32 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth  } from '../../utils/firebase/firebase.utils'
+import { getRedirectResult } from 'firebase/auth';
+import { useEffect } from 'react';
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+    // useEffect(() => {
+    //     doSomething();
+    // }, []);
+    // const doSomething = async () => {
+    //     const response = await getRedirectResult(auth);
+    //     console.log(response);
+    // }
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
     }
-
+    const logGoogleRedirectUser = async () => {
+        const { user } = await signInWithGoogleRedirect();
+        console.log({user})
+    }
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
+            </button>
+            <button onClick={logGoogleRedirectUser}>
+                Sign in with Google Redirect
             </button>
         </div>
     )
